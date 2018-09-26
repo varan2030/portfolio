0eb8f590629b8fa82459952670b19ef2c9773ceb
varan2030 <varan2030@>
2018-09-26 09:01:33 -0400

    upload

---
+++ src/js/main.js
@@ -0,0 +1,15 @@
+$('[data-toggle="tooltip"]').tooltip({
+  'placement': 'top'
+});
+$('[data-toggle="popover"]').popover({
+  trigger: 'hover',
+      'placement': 'top'
+});
+
+$('#userNameField').tooltip({
+  'show': true,
+      'placement': 'bottom',
+      'title': "Please remember to..."
+});
+
+$('#userNameField').tooltip('show');
---
+++ src/mail.php
@@ -0,0 +1,41 @@
+<?php 
+
+require_once('phpmailer/PHPMailerAutoload.php');
+$mail = new PHPMailer;
+$mail->CharSet = 'utf-8';
+
+$name = $_POST['name'];
+$email = $_POST['email'];
+$phone = $_POST['phone'];
+$message = $_POST['message'];
+
+//$mail->SMTPDebug = 3;                               // Enable verbose debug output
+
+$mail->isSMTP();                                      // Set mailer to use SMTP
+$mail->Host = 'smtp.mail.ru';  																							// Specify main and backup SMTP servers
+$mail->SMTPAuth = true;                               // Enable SMTP authentication
+$mail->Username = 'varan2030@mail.ru'; // Ваш логин от почты с которой будут отправляться письма
+$mail->Password = 'Elvira123'; // Ваш пароль от почты с которой будут отправляться письма
+$mail->SMTPSecure = 'ssl';                            // Enable TLS encryption, `ssl` also accepted
+$mail->Port = 465; // TCP port to connect to / этот порт может отличаться у других провайдеров
+
+$mail->setFrom('varan2030@mail.ru'); // от кого будет уходить письмо?
+$mail->addAddress('almaz.dolubaev@gmail.com');     // Кому будет уходить письмо 
+//$mail->addAddress('ellen@example.com');               // Name is optional
+//$mail->addReplyTo('info@example.com', 'Information');
+//$mail->addCC('cc@example.com');
+//$mail->addBCC('bcc@example.com');
+//$mail->addAttachment('/var/tmp/file.tar.gz');         // Add attachments
+//$mail->addAttachment('/tmp/image.jpg', 'new.jpg');    // Optional name
+$mail->isHTML(true);                                  // Set email format to HTML
+
+$mail->Subject = 'Message from Portfolio site';
+$mail->Body    = '' .$name . ' sent massege to you ' .$phone. '<br>Email: ' .$email '<br>' .$message;
+$mail->AltBody = '';
+
+if(!$mail->send()) {
+    echo 'Error';
+} else {
+    header('location: thank-you.html');
+}
+?>
